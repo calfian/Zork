5b69ed0820eefcf29ef903fa22d48cf07332f1b3
calfian <chrisalfian@knights.ucf.edu>
2020-09-09 11:58:00 -0400

    Zork p 1.3
    Zork p 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Zork
 {
+
+   enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            Commands command = Commands.UNKNOWN;
+
+            
+
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for Playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                        outputString = "You moved North";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved South";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved East";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved WEST";
+                        break;
+
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+                
+                Console.WriteLine(outputString);
+            }
+
+
+            
         }
+
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+        
     }
 }
